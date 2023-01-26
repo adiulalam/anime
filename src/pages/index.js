81ea1a6ac61f6e1b476ece2e22034c5d6ebff382
Adiul Alam Adil <adiulalam@gmail.com>
2023-01-26 21:48:42 +0000

    css

--- src/pages/index.js
+++ src/pages/index.js
@@ -47,6 +47,41 @@ export default function Home() {
 
 	// console.log(data);
 
+	const statusMap = {
+		FINISHED: {
+			name: "Finished",
+			style: "bg-violet-400 dark:bg-violet-900",
+		},
+		RELEASING: {
+			name: "Releasing",
+			style: "bg-teal-400 dark:bg-teal-800",
+		},
+		NOT_YET_RELEASED: {
+			name: "Not Released",
+			style: "bg-indigo-400 dark:bg-indigo-800",
+		},
+		CANCELLED: {
+			name: "Cancelled",
+			style: "bg-pink-400 dark:bg-bg-pink-800",
+		},
+	};
+
+	const scoreColour = (score) =>
+		score >= 80
+			? "bg-green-400 dark:bg-green-900"
+			: score >= 60 && score < 80
+			? "bg-yellow-400 dark:bg-yellow-700"
+			: score < 60
+			? "bg-red-400 dark:bg-red-800"
+			: "bg-gray-300 dark:bg-gray-800";
+
+	const seasonalMap = {
+		WINTER: "bg-sky-400 dark:bg-sky-900",
+		SPRING: "bg-lime-400 dark:bg-lime-800",
+		SUMMER: "bg-rose-500 dark:bg-rose-800",
+		FALL: "bg-orange-400 dark:bg-orange-800",
+	};
+
 	return (
 		<div className="bg-white dark:bg-black">
 			<Switcher />
@@ -64,8 +99,8 @@ export default function Home() {
 				</div>
 				<Slider {...settings} ref={slider}>
 					{data?.popular?.media?.map((e, i) => (
-						<div key={i} className="p-2">
-							<div className="group relative ">
+						<div key={i} className="group p-2">
+							<div className=" relative ">
 								<Link href={`/${e.id}`}>
 									<div
 										className={`flex flex-col md:h-72 md:w-52 h-52 w-36 justify-end bg-cover bg-no-repeat rounded-lg`}
@@ -78,7 +113,7 @@ export default function Home() {
 												: setShowRight(true)
 										}
 									>
-										<div className="flex md:max-h-12 max-h-10 text-sm md:text-base font-medium w-full text-ellipsis overflow-hidden rounded-lg backdrop-blur-md backdrop-contrast-50">
+										<div className="flex md:max-h-14 max-h-10 text-sm md:text-lg font-medium w-full text-ellipsis overflow-hidden rounded-lg backdrop-blur-md backdrop-contrast-50">
 											<h3 className="px-2 text-black [text-shadow:_0_1px_0_rgb(255_255_255_/_40%)] dark:text-white dark:[text-shadow:_0_1px_0_rgb(0_0_0_/_40%)]">
 												{e.title.userPreferred}
 											</h3>
@@ -87,7 +122,7 @@ export default function Home() {
 								</Link>
 
 								<div
-									className={`hidden group-hover:flex flex-col items-center justify-center hover:fixed absolute top-0 z-10 ${
+									className={`hidden group-hover:flex flex-col items-center justify-evenly absolute top-0 z-10 ${
 										showRight ? "left-36 md:left-52 pl-5 pr-2" : "right-36 md:right-52 pr-5 pl-2"
 									} flex-col md:h-72 md:w-56 h-52 w-40 rounded-lg transition-all duration-300 ease-in-out`}
 									style={{
@@ -102,23 +137,35 @@ export default function Home() {
 										backgroundColor: e.coverImage.color ?? "#9CA38F",
 									}}
 								>
-									<div className="flex w-full h-auto justify-center items-center mt-2 ">
-										<div className="flex w-full h-auto items-center justify-center bg-slate-600 break-normal rounded-lg">
-											{e?.format}
+									{(e?.status || e?.averageScore) && (
+										<div className="flex flex-row w-full h-auto justify-center items-center gap-2 ">
+											{e.status && (
+												<div
+													className={`${
+														statusMap[e.status].style ?? "bg-gray-300 dark:bg-gray-800"
+													} flex w-auto py-1 px-2 h-auto items-center justify-center break-normal rounded-lg text-black dark:text-white text-xs md:text-base`}
+												>
+													{statusMap[e.status].name}
+												</div>
+											)}
+											{e.averageScore && (
+												<div
+													className={`${scoreColour(
+														e.averageScore
+													)} flex w-8 h-8 md:w-12 md:h-12 items-center justify-center rounded-full text-black dark:text-white text-xs md:text-base`}
+												>
+													{e.averageScore}
+												</div>
+											)}
 										</div>
-									</div>
+									)}
 
-									<div className="flex flex-row w-full h-auto justify-center items-center mt-2 gap-2 ">
-										<div className="flex w-24 h-auto items-center justify-center bg-slate-400 break-normal rounded-lg">
-											{e.status}
-										</div>
-										<div className="flex w-10 h-10 items-center justify-center bg-slate-400 rounded-full">
-											{e.averageScore}
-										</div>
-									</div>
-
-									<div className="flex w-full h-auto justify-center items-center mt-2 ">
-										<div className="flex w-full h-auto items-center justify-center bg-slate-600 break-normal rounded-lg">
+									<div className="flex w-full h-auto justify-center items-center">
+										<div
+											className={`${
+												e.season ? seasonalMap[e.season] : "bg-gray-300 dark:bg-gray-800"
+											} flex w-auto h-auto items-center justify-center break-normal rounded-lg text-black dark:text-white py-1 px-2 text-xs md:text-base`}
+										>
 											{e?.nextAiringEpisode?.id
 												? `Episode ${e.nextAiringEpisode.episode} in ${humanizeDuration(
 														(e.nextAiringEpisode.timeUntilAiring * 1000).toFixed(0) ?? 0,
@@ -132,18 +179,18 @@ export default function Home() {
 									</div>
 
 									{e.episodes && (
-										<div className="flex w-full h-auto justify-center items-center mt-2 ">
-											<div className="flex w-full h-auto items-center justify-center bg-slate-400 break-normal rounded-lg">
+										<div className="flex w-full h-auto justify-center items-center">
+											<div className="flex py-1 px-2 md:px-4 h-auto items-center justify-center bg-slate-400 dark:bg-slate-800 text-black dark:text-white break-normal rounded-lg text-xs md:text-base">
 												Total Episodes: {e.episodes}
 											</div>
 										</div>
 									)}
 
-									<div className="flex flex-row flex-wrap w-auto h-auto justify-center items-center mt-2">
+									<div className="flex flex-row flex-wrap w-auto h-auto justify-center items-center ">
 										{e?.genres?.slice(0, 5)?.map((e, i) => (
 											<div
 												key={i}
-												className="flex w-auto h-auto items-center justify-center bg-slate-600 break-normal rounded-lg m-1 px-1"
+												className="flex w-auto h-auto items-center justify-center bg-gray-300 dark:bg-gray-700 text-black dark:text-white break-normal rounded-3xl m-1 px-2 text-xs md:text-base"
 											>
 												{e}
 											</div>
