22f2d6dac035752fef28def18b67290aabf501d7
Adiul Alam Adil <adiulalam@gmail.com>
2023-01-29 02:10:16 +0000

    cu

--- src/pages/error.jsx
+++ src/pages/error.jsx
@@ -7,15 +7,15 @@ import { Fade } from "@/utils/getAnimationStyle";
 export default function PageError({ message, statuscode }) {
 	return (
 		<div
-			className="flex items-center flex-col flex-wrap gap-2 pb-20 justify-center h-screen m-0 text-center 
+			className="flex items-center flex-col flex-wrap gap-2 px-4 justify-center h-screen m-0 text-center 
 		w-full font-bold text-8xl text-neutral-600 dark:text-neutral-200 bg-neutral-200 dark:bg-neutral-600"
 		>
 			<Switcher />
 			<div className="flex h-64 w-80 md:w-96 md:h-80 relative">
 				<Image alt="zoro" src={zoro} fill className="rounded-lg" />
 			</div>
-			<Fade className="text-5xl md:text-8xl pr-3 w-2/3 h-auto text-clip">{`Error ${
-				statuscode ? `${statuscode}` : ""
+			<Fade className="text-5xl md:text-8xl pr-3 w-fit h-auto text-clip">{`Error ${
+				statuscode ? `${statuscode}` : "Unknown"
 			} ${message ? `- ${message}` : ""}`}</Fade>
 		</div>
 	);
