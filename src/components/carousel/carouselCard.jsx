8521b06b178ff6b2aac3a2c2ae480ac37b6f57e6
adiul alam adil <aadil@my1ps.com>
2023-02-08 16:48:59 +0000

    performance

--- src/components/carousel/carouselCard.jsx
+++ src/components/carousel/carouselCard.jsx
@@ -1,13 +1,10 @@
-import Color from "color";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
-import Skeleton from "react-loading-skeleton";
 import { CarouselHover } from "./carouselHover";
 
 export const CarouselCard = ({ cardData, carouselWidth, slider }) => {
 	const [showRight, setShowRight] = useState(false);
-	const [isLoading, setIsLoading] = useState(true);
 
 	return (
 		<div className="p-2">
@@ -29,19 +26,6 @@ export const CarouselCard = ({ cardData, carouselWidth, slider }) => {
 						}}
 						onTouchEnd={() => slider.current.slickPlay()}
 					>
-						{isLoading && (
-							<div className="absolute h-full w-full rounded-xl bottom-1">
-								<Skeleton
-									className="h-full w-full"
-									baseColor={`${cardData.coverImage.color ?? "#202020"}`}
-									highlightColor={`${
-										cardData.coverImage.color
-											? Color(cardData.coverImage.color).rotate(90).hex()
-											: "#4444"
-									}`}
-								/>
-							</div>
-						)}
 						<Image
 							alt={cardData.title.userPreferred}
 							src={cardData.coverImage.large}
@@ -49,15 +33,14 @@ export const CarouselCard = ({ cardData, carouselWidth, slider }) => {
 							sizes="100%"
 							className="rounded-lg"
 							object-fit="cover"
-							onLoadingComplete={() => setIsLoading(false)}
 						/>
 						<div
-							className={`flex justify-center md:max-h-14 max-h-10 text-sm md:text-lg font-medium w-full 
-											text-ellipsis overflow-hidden rounded-lg backdrop-blur-md backdrop-contrast-50 
+							className={`flex justify-center md:max-h-14 max-h-10 text-sm md:text-lg font-medium w-full
+											text-ellipsis overflow-hidden rounded-lg backdrop-blur-md backdrop-contrast-50
 											backdrop-saturate-200 backdrop-brightness-200 dark:backdrop-brightness-75`}
 						>
 							<h3
-								className="px-2 text-center text-black [text-shadow:_0_1px_0_rgb(255_255_255_/_40%)] 
+								className="px-2 text-center text-black [text-shadow:_0_1px_0_rgb(255_255_255_/_40%)]
 											dark:text-white dark:[text-shadow:_0_1px_0_rgb(0_0_0_/_40%)]"
 							>
 								{cardData.title.userPreferred}
