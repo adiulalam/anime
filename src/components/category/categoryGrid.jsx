2ae5c58b6a4d807ad6f8cff9e6800c57f81cbbd9
Adiul Alam Adil <adiulalam@gmail.com>
2023-02-13 22:59:06 +0000

    css

--- src/components/category/categoryGrid.jsx
+++ src/components/category/categoryGrid.jsx
@@ -6,11 +6,10 @@ const CategoryGrid = () => {
 			<div class="flex h-48 md:h-56 sm:h-40 w-full md:w-[28rem] sm:w-72 justify-center text-6xl border-2 border-gray-300 rounded-xl bg-gray-100">
 				<div className="grid grid-cols-5 grid-rows-5 grid-flow-col w-full h-full">
 					<div className="col-span-2 row-span-full">
-						<div className="relative w-full h-full bg-contain ">
+						<div className="relative w-full h-full bg-cover">
 							<Image
 								alt="one piece"
 								fill
-								object-fit="contain"
 								src="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/nx21-tXMN3Y20PIL9.jpg"
 								className="rounded-l-xl"
 							/>
