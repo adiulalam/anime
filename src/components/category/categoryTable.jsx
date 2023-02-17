8268f39f520525935d2e4b02be7d4dc50ddca898
Adiul Alam Adil <adiulalam@gmail.com>
2023-02-17 20:56:34 +0000

    css

--- src/components/category/categoryTable.jsx
+++ src/components/category/categoryTable.jsx
@@ -14,40 +14,30 @@ const CategoryTable = () => {
 					className="object-contain"
 				/>
 			</div>
-			<div className="flex flex-grow bg-slate-500 ">
-				<div className="flex flex-col md:flex-row h-full w-full bg-blue-600">
-					<div className="flex basis-1/2 md:h-1/1 h-1/1 md:w-1/2 w-1/1 bg-red-600">
-						<div className="flex flex-col h-full w-full bg-purple-600">
-							<div className="flex basis-1/2 h-1/1 w-1/1 bg-orange-500">
-								<p>hello</p>
-							</div>
-							<div className="flex basis-1/2 h-1/1 w-1/1 bg-orange-600">
-								<p>hello</p>
-							</div>
+			<div className="flex flex-col md:flex-row flex-grow overflow-hidden bg-slate-500 ">
+				<div className="flex flex-col md:basis-1/2 basis-2/3 bg-red-600">
+					<div className="flex basis-1/2 items-center bg-orange-500">
+						<p>Title</p>
+					</div>
+					<div className="flex basis-1/2 items-center bg-orange-600">
+						<p>genres</p>
+					</div>
+				</div>
+				<div className="flex flex-row md:basis-1/2 basis-1/3 bg-red-800">
+					<div className="flex basis-3/5 md:basis-1/2 md:flex-col flex-row bg-pink-600">
+						<div className="flex basis-1/3 md:basis-1/2 bg-pink-500">
+							<p>Score</p>
+						</div>
+						<div className="flex basis-2/3 md:basis-1/2 bg-pink-600">
+							<p>Next Episode / Season Year</p>
 						</div>
 					</div>
-					<div className="flex basis-1/2 md:h-1/1 h-1/1 md:w-1/2 w-1/1 bg-red-800">
-						<div className="flex flex-row h-full w-full bg-purple-600">
-							<div className="flex basis-1/2 md:h-1/1 h-1/1 md:w-1/2 w-1/1 bg-pink-600">
-								<div className="flex md:flex-col flex-row h-full w-full bg-purple-600">
-									<div className="flex basis-1/2 h-1/1 w-1/1 bg-pink-500">
-										<p>hello</p>
-									</div>
-									<div className="flex basis-1/2 h-1/1 w-1/1 bg-pink-600">
-										<p>hello</p>
-									</div>
-								</div>
-							</div>
-							<div className="flex basis-1/2 md:h-1/1 h-1/1 md:w-1/2 w-1/1 bg-pink-800">
-								<div className="flex md:flex-col flex-row h-full w-full bg-purple-600">
-									<div className="flex basis-1/2 h-1/1 w-1/1 bg-pink-700">
-										<p>hello</p>
-									</div>
-									<div className="flex basis-1/2 h-1/1 w-1/1 bg-pink-900">
-										<p>hello</p>
-									</div>
-								</div>
-							</div>
+					<div className="flex basis-2/5 md:basis-1/2 md:flex-col flex-row bg-pink-800">
+						<div className="flex basis-1/2 bg-pink-700">
+							<p>Format</p>
+						</div>
+						<div className="flex basis-1/2 bg-pink-900">
+							<p>Airing Status</p>
 						</div>
 					</div>
 				</div>
