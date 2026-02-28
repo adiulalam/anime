5fffa83c173f0231b06e713f3f0a18712b528f89
Adiul Alam Adil <adiulalam@gmail.com>
2026-02-28 21:41:39 +0000

    fix: add correct docker compose command

--- deploy-anime.sh
+++ deploy-anime.sh
@@ -4,4 +4,4 @@ cd anime/ && echo 'Inside anime Directory'
 
 git reset --hard HEAD && git pull origin main && echo 'pulled master main'
 
-docker-compose up -d --build --force-recreate && echo 'Container up and Running'
+docker compose up -d --build --force-recreate && echo 'Container up and Running'
