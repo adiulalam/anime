93c24dbd77affd3216327dc5075b53bcdff5bad5
Adiul Alam Adil <adiulalam@gmail.com>
2026-02-28 21:36:49 +0000

    fix: limit cache to 7 days

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		domains: ["s4.anilist.co"],
-	},
-	compiler: {
-		styledComponents: true,
-	},
-	output: "standalone",
-	// reactStrictMode: true,
+  images: {
+    domains: ["s4.anilist.co"],
+    minimumCacheTTL: 604800, // 7 days in seconds
+  },
+  compiler: {
+    styledComponents: true,
+  },
+  output: "standalone",
+  // reactStrictMode: true,
 };
 
 module.exports = nextConfig;
